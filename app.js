ebe22d5a780b9829ef0f8a5717eb2e2307041854
nick567187 <nchang.edu@gmail.com>
2018-07-20 10:56:57 -0700

    on the way to MVP

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+//attach event listener to buttons
+//create function stub for read/write/delete
+  //research local storage
+  // $().on('click', function() {
+
+  // });
+
+  $().on('click', function() {
+
+  });
+
+
+});
