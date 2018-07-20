c84c80de931a362989010917b0d29241e3f6da1e
nick567187 <nchang.edu@gmail.com>
2018-07-20 11:30:38 -0700

    finished hardcoded event for get set and delete

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
 //attach event listener to buttons
 //create function stub for read/write/delete
   //research local storage
-  // $().on('click', function() {
 
-  // });
-
-  $().on('click', function() {
-
+  $('.store-btn').on('click', function() {
+    localStorage.setItem("hrext", "three is the best")
   });
+
+  $('.get-btn').on('click', function() {
+    console.log(localStorage.getItem("hrext"));
+  });
+
+  $('.delete-btn').on('click', function() {
+    localStorage.removeItem("hrext");
+  });
+
 
 
 });
