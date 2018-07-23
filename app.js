9155156c158e867d6485013b18f5e3c398fa0293
nick567187 <nchang.edu@gmail.com>
2018-07-23 15:50:58 -0700

    finished all basic functions for a program that writes affirmations for myself and says them to me

--- app.js
+++ app.js
@@ -10,45 +10,43 @@ $(document).ready(function() {
   $('.store-btn').on('click', function() {
   	let titleValue = $('.input-field-title').val();
   	let contentValue = $('.input-field-body').val();
+  	if(titleValue === '') {
+  	$('.speech').html("<p> Hurry and give this a title! I can't wait to affirm you! </p>")
+  	} else if(contentValue === '') {
+    $('.speech').html("<p> Come on...fill the content box with <em>stellar</em> affirmations!</p>")
+  	} else {
     localStorage.setItem(titleValue, contentValue)
-    $('.speech').html(`<p>You're <strong>'${titleValue}'</strong> secret is safe with me ;)</p>`)
+    $('.speech').html(`<p>I can't wait to tell you how good you are at <strong>'${titleValue}'</strong> ;)</p>`)
+    }
     $('.speech').css('display', 'block')
   });
 
   $('.get-btn').on('click', function() {
     let titleValue = localStorage.getItem($('.input-field-title').val())
-    console.log(titleValue)
-    if(titleValue === '') {
-       $('.speech').html("<p>You haven't coded this yet, silly</p>")
+    let text = $('.input-field-title').val()
+    if(titleValue === null) {
+       $('.speech').html(`<p>You haven't told me how good you are at ${text} yet</p>`)
        $('.speech').css('display', 'block')	
     } else {
     $('.speech').html(`<p>${titleValue}</p>`)
-    $('.speech').css('display', 'block')
+    $('.speech').css('display', 'flex')
     }
   });
 
   $('.delete-btn').on('click', function() {
     let titleValue = $('.input-field-title').val()
     if(localStorage.getItem(titleValue) === null) {
-      $('.speech').html("<p>You haven't coded this yet, silly</p>")
+      $('.speech').html(`<p>You haven't told me how good you are at ${text} yet</p>`)
       $('.speech').css('display', 'block')	
     } else {
     	$('.speech').html("<div class='dialogTitle'></div><input type='button' value='yes' id='btnYes' /> <input type='button' value='no' id='btnNo'/>")
     	$('.speech').css('display', 'block')
     	confirm("Are you sure you want me to forget this?", function() {
     	  localStorage.removeItem(titleValue);	
-          $('.speech').html(`<p>I didn't care about <strong>'${titleValue}'</strong> anyway</p>`)	
+          $('.speech').html(`<p>It's not that important to be good at <strong>'${titleValue}'</strong> anyway</p>`)	
     	}, function() {
     	  $('.speech').html("<p>I'll keep this safe for you for now</p>")
     	});
-      // let result = window.confirm("Are you sure you want to delete this?");
-      // result;
-      // if(result) {
-      //   localStorage.removeItem(titleValue);	
-      //   $('.speech').html(`<p>I didn't care about <strong>'${titleValue}'</strong> anyway</p>`)
-      // }
-      //   $('.speech').html("<p>I'll keep this safe for you for now</p>")
-      //   $('.speech').css('display', 'block')
     }
     
     function confirm(message, yes, no) {
